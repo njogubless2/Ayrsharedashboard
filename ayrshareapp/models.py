20b9a35c71a32bdca9ee03c1b939c3de33f672ec
njogubless <pnjogubless@gmail.com>
2023-09-12 18:02:40 +0300

    writing the account creation models

--- ayrshareapp/models.py
+++ ayrshareapp/models.py
@@ -1,26 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class CLient (models.Model):
-    name= models.CharField(max_length=200)
-    email= models.EmailField(max_length=255)
-    password= models.CharField(max_length=150)
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
 
+# Custom User Model
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length= 128)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
 
-class SocialMediaPlatform (models.Model):
-    name= models.CharField(max_length=50)
-    ayrshare_api_key=models.CharField(max_length=100)
-    ayrshare_api_secret=models.CharField(max_length=100)
-    
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
     def __str__(self):
-        return self.name
-
-class ScheduledPost(models.Model):
-    content = models.TextField()
-    Scheduled_datetime=models.DateTimeField()
-    social_media_platform=models.ForeignKey(social_media_platform, on_delete=models.CASCADE)
-    is_published=models.BooleanField(default=False)
-    
-    def __str__(self):
-        return f"Post '{self.content[:20]}..' scheduled for{self.Scheduled_datetime} on { self.social_media_platform}"
-    
+        return self.email
