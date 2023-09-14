40bf5a3d6bd0388c39a54567bbb7d02e8ca0a3e4
njogubless <pnjogubless@gmail.com>
2023-09-14 21:35:03 +0300

    rewriting the models.py and writing the test.py

--- ayrshareapp/models.py
+++ ayrshareapp/models.py
@@ -1,35 +1,52 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import User  # You may need to adjust the user model if you have a custom user model
 
-# Custom User Manager
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
-
-# Custom User Model
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length= 128)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name','email','password']
+# User Profile Model
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    social_media_platforms = models.ManyToManyField('SocialMediaPlatform')
 
     def __str__(self):
-        return self.email
+        return self.user.username
+
+# Social Media Platform Model
+class SocialMediaPlatform(models.Model):
+    name = models.CharField(max_length=50)
+    icon = models.ImageField(upload_to='platform_icons/')
+
+    def __str__(self):
+        return self.name
+
+# Content Model
+class Content(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content_text = models.TextField()
+    image = models.ImageField(upload_to='content_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+# Scheduled Post Model
+class ScheduledPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    platform = models.ForeignKey(SocialMediaPlatform, on_delete=models.CASCADE)
+    scheduled_datetime = models.DateTimeField()
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Scheduled Post by{self.user.username} at {self.scheduled_datetime}"
+
+# Analytics Model
+class PostAnalytics(models.Model):
+    scheduled_post = models.ForeignKey(ScheduledPost, on_delete=models.CASCADE)
+    views = models.PositiveIntegerField()
+    likes = models.PositiveIntegerField()
+    comments = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Analytics for {self.scheduled_post}"
--- ayrshareapp/tests.py
+++ ayrshareapp/tests.py
@@ -1,3 +1,90 @@
+import unittest
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.utils import timezone
+from .models import ScheduledPost
+from .views import perform_api_integration
 
-# Create your tests here.
+class ScheduledPostModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.scheduled_datetime = timezone.now() + timezone.timedelta(hours=1)
+        self.scheduled_post = ScheduledPost.objects.create(
+            user=self.user,
+            content='Test content',
+            scheduled_datetime=self.scheduled_datetime,
+        )
+
+    def test_scheduled_post_creation(self):
+        self.assertEqual(self.scheduled_post.user, self.user)
+        self.assertEqual(self.scheduled_post.content, 'Test content')
+        self.assertEqual(self.scheduled_post.scheduled_datetime, self.scheduled_datetime)
+        self.assertFalse(self.scheduled_post.is_published)
+
+class DashboardViewTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_dashboard_view_with_authenticated_user(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('dashboard'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'dashboard.html')
+
+    def test_dashboard_view_with_unauthenticated_user(self):
+        response = self.client.get(reverse('dashboard'))
+        self.assertEqual(response.status_code, 302)  # Redirects to login page
+
+class SchedulePostViewTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_schedule_post_view_with_authenticated_user(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('schedule_post'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'schedule_post.html')
+
+    def test_schedule_post_view_with_unauthenticated_user(self):
+        response = self.client.get(reverse('schedule_post'))
+        self.assertEqual(response.status_code, 302)  # Redirects to login page
+
+    def test_schedule_post_api_integration_success(self):
+        self.client.login(username='testuser', password='testpassword')
+        data = {
+            'content': 'Test content',
+            'scheduled_datetime': timezone.now() + timezone.timedelta(hours=1),
+        }
+        response = self.client.post(reverse('schedule_post'), data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'success_view')  # Replace with your actual success view
+
+    def test_schedule_post_api_integration_failure(self):
+        self.client.login(username='testuser', password='testpassword')
+        data = {
+            'content': 'Test content',
+            'scheduled_datetime': timezone.now() + timezone.timedelta(hours=1),
+        }
+        # Mock the perform_api_integration function to simulate API failure
+        with unittest.mock.patch('yourapp.views.perform_api_integration', return_value=False):
+            response = self.client.post(reverse('schedule_post'), data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'form', 'content', 'Error message')  # Add your error message here
+
+    # You can add more test cases as needed
+
+class PerformApiIntegrationTestCase(TestCase):
+    def test_api_integration_success(self):
+        content = 'Test content'
+        scheduled_datetime = timezone.now() + timezone.timedelta(hours=1)
+        self.assertTrue(perform_api_integration(content, scheduled_datetime))
+
+    def test_api_integration_failure(self):
+        content = 'Test content'
+        scheduled_datetime = timezone.now() + timezone.timedelta(hours=1)
+        # Mock the requests.post method to simulate API failure
+        with unittest.mock.patch('requests.post', side_effect=Exception('API Error')):
+            self.assertFalse(perform_api_integration(content, scheduled_datetime))
+
+    # Add more test cases for edge cases and different API responses as needed
